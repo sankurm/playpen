f34efa6af5fc50ecd4a189aee263e11194bae590
Ankur Satle <sankurm@users.noreply.github.com>
2019-05-13 19:13:05 +0530

    Moved function bodies out of the classes

--- avro-wrapper/avro-wrapper.h
+++ avro-wrapper/avro-wrapper.h
@@ -20,27 +20,23 @@ namespace AVRO
 {
 	using owning_buffer = std::pair<std::unique_ptr<uint8_t[]>, int>;
 	using SchemaType = CdrType;
+	using SchemaRegister = std::map<SchemaType, Schema>;
 	
 	class SchemaRegistry
 	{
-		SchemaRegistry(const std::map<SchemaType, Schema>& _registry) : registry(_registry) {}
-		SchemaRegistry(std::map<SchemaType, Schema>&& _registry) : registry(_registry) {}
-		SchemaRegistry(const SchemaRegistry& other) : registry(other.registry) {}
-		SchemaRegistry(SchemaRegistry&& other) : registry(other.registry) {}
+		SchemaRegistry(const SchemaRegister& _registry);
+		SchemaRegistry(SchemaRegister&& _registry);
+		SchemaRegistry(const SchemaRegistry& other);
+		SchemaRegistry(SchemaRegistry&& other);
 		
-		void register(SchemaType type, const Schema& schema) { registry[type] = schema; }
-		void register(SchemaType type, Schema&& schema) { registry[type] = schema; }
-		const Schema& find(SchemaType type) const {
-			if (auto it = registry.find(type); it != end(registry)) {
-				return it->second;
-			}
-			return avro::NullSchema{};
-		}
-		void unregister(SchemaType type) { registry.erase(type); }
+		void reg(SchemaType type, const Schema& schema);
+		void reg(SchemaType type, Schema&& schema);
+		const Schema& find(SchemaType type) const;
+		void unreg(SchemaType type);
 		
 		private:
 		//TODO: Should this be ValidSchema instead? 
-		std::map<SchemaType, Schema> registry;
+		SchemaRegistry registry;
 	};
 	
 	class AvroEncoder
@@ -48,27 +44,14 @@ namespace AVRO
 		AvroEncoder(const Schema& schema);
 		
 		template<typename T>
-		owning_buffer encode(const T& data, const dictionary& dict) const {
-			EncoderPtr encoder = binaryEncoder();
-			auto out = memoryOutputStream(8096);
-			//GenericWriter writer{validSchema, encoderPtr};
-			StreamWriter writer{out};	//encoder not used!!!???
-			encodeFields(writer, data, dict);
-			return writer.getBuffer();
-		}
+		owning_buffer encode(const T& data, const dictionary& dict) const;
 		
 		private:
 			template<typename T>
-			void encodeFields(GenericWriter& writer, const T& data, const dictionary& dict) const {
-				const int no_fields = schema.root().names();
-				for (int i = 0; i < no_names; i++) {
-					const std::string& fieldName = schema.root().nameAt(i);
-					const DataGetter& getter = dict.find(fieldName);
-					GenericDatum value = getter(data);
-					writer.write(value);
-				}
-				writer.flush();
-			}
+			void encodeHeader(GenericWriter& writer, const T& data, const dictionary& dict) const;
+			template<typename T>
+			void encodeFields(GenericWriter& writer, const T& data, const dictionary& dict) const;
+		
 			Schema schema;
 	};
   
@@ -82,15 +65,69 @@ namespace AVRO
 		const Schema& schema = schemaRegistry.find(type);
 		return avro_encode(data, dict, schema);
 	}
+}
+
+template<typename T>
+inline AVRO::owning_buffer AVRO::AvroEncoder::encode(const T& data, const dictionary& dict) const {
+	EncoderPtr encoder = binaryEncoder();
+	auto out = memoryOutputStream(8096);
+	//GenericWriter writer{validSchema, encoderPtr};
+	StreamWriter writer{out};	//encoder not used!!!???
+	encodeHeader(writer, data, dict);
+	encodeFields(writer, data, dict);
+	return writer.getBuffer();
+}
+template<typename T>
+inline void AVRO::AvroEncoder::encodeHeader(GenericWriter& writer, const T& data, const dictionary& dict) const {
+}
+template<typename T>
+inline void AVRO::AvroEncoder::encodeFields(GenericWriter& writer, const T& data, const dictionary& dict) const {
+	const int no_fields = schema.root().names();
+	for (int i = 0; i < no_names; i++) {
+		const std::string& fieldName = schema.root().nameAt(i);
+		const DataGetter& getter = dict.find(fieldName);
+		GenericDatum value = getter(data);
+		writer.write(value);
+	}
+	writer.flush();
+}
+
+inline AVRO::SchemaRegistry::SchemaRegistry(const SchemaRegister& _registry) : 
+registry(_registry) {
+}
+inline AVRO::SchemaRegistry::SchemaRegistry(SchemaRegister&& _registry) : 
+registry(_registry) {
+}
+inline AVRO::SchemaRegistry::SchemaRegistry(const SchemaRegistry& other) : 
+registry(other.registry) {
+}
+inline AVRO::SchemaRegistry::SchemaRegistry(SchemaRegistry&& other) : 
+registry(other.registry) {
+}
+
+inline void AVRO::SchemaRegistry::reg(SchemaType type, const Schema& schema) { 
+	registry[type] = schema; 
+}
+inline void AVRO::SchemaRegistry::reg(SchemaType type, Schema&& schema) { 
+	registry[type] = schema; 
+}
+inline const Schema& AVRO::SchemaRegistry::find(SchemaType type) const {
+	if (auto it = registry.find(type); it != end(registry)) {
+		return it->second;
+	}
+	return avro::NullSchema{};
+}
+inline void AVRO::SchemaRegistry::unreg(SchemaType type) { 
+	registry.erase(type); 
+}
+
 #endif
 
-	//Caller to be owner of returned buffer - _MUST_ free
+#ifndef __cplusplus
+//Caller to be owner of returned buffer - _MUST_ free
 	EXTERN_C uint8_t* avro_encode(void* data, dictionary* dict, SchemaType type) {
 		return avro_encode(reinterpret_cast<const T_CS_CDR_MSG&>(*data), *dict, type).first.release();
 	}
-  
-#ifdef __cplusplus
-}
 #endif
-
+  
 #endif  //_AVRO_WRAPPER_H_
