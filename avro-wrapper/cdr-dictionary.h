ed9b27f9b22f5bae9bbab0c1db081e7d94bdd0b6
Ankur Satle <sankurm@users.noreply.github.com>
2019-05-13 19:35:10 +0530

    Usage examples for DataGetters

--- avro-wrapper/cdr-dictionary.h
+++ avro-wrapper/cdr-dictionary.h
@@ -3,32 +3,53 @@
 
 #include "cdr.h"
 
+#include "avro"
+
 #include <map>
 #include <functional>
 #include <string>
 
-using DataGetter = std::functional<GenericDatum(const Cdr&)>;
-using DataGetterMap = std::map<std::string, DataGetter>;
-
-DataGetterMap dataGetterMap = {
-	{"timestamp"s, {[](const Cdr& cdr) { return GenericDatum{cdr.timestamp}; }}},
-	{"diameter.application-id"s, {[](const Cdr& cdr) { 
-		return cdr.type == Diameter? GenericDatum{cdr.detail.app_id}: GenericDatum{}; }}},
-	{"isup.called-no"s, {[](const Cdr& cdr) { 
-		return cdr.type == Isup? GenericDatum{cdr.detail.called_no}: GenericDatum{}; }}},
-};
-
-//DataGetter Dictionary for Cdr
-class CdrDictionary
+namespace AVRO
 {
-	CdrDictionary(DataGetterMap& m) : dataGetterMap(m) {}
-	const DataGetter& find(const std::string& field_name) const {
-		if (auto it = dataGetterMap.find(field_name); it != end(dataGetterMap)) {
-			return it->second;
-		}
-		static DataGetter nullGetter{[](const Cdr&) { return GenericDatum{}; }};
-		return nullGetter;
+	using DataGetter = std::functional<GenericDatum(const Cdr&)>;
+	using DataGetterMap = std::map<std::string, DataGetter>;
+
+	//DataGetters
+	auto diameter_app_id_getter = [](const Cdr& cdr) -> GenericDatum {
+		return cdr.type == Diameter? 
+			GenericDatum{cdr.detail.app_id}:
+			GenericDatum{};
+	};
+	GenericDatum isup_caller_no_getter(const Cdr& cdr) {
+		return cdr.type == Isup? 
+			GenericDatum{cdr.detail.caller_no}: 
+			GenericDatum{};
 	}
-};
+
+	//The lookup of the DataGetter against the Avro field name
+	DataGetterMap dataGetterMap = {
+		{"timestamp"s, {[](const Cdr& cdr) { return GenericDatum{cdr.timestamp}; }}},
+		{"diameter.application-id"s, {diameter_app_id_getter}},
+		{"isup.caller-no"s, {isup_caller_no_getter}},
+		{"isup.called-no"s, {[](const Cdr& cdr) { 
+			return cdr.type == Isup? GenericDatum{cdr.detail.called_no}: GenericDatum{}; }}},
+	};
+
+	//DataGetter Dictionary Abstraction for Cdr
+	class CdrDictionary
+	{
+		CdrDictionary(const DataGetterMap& m) : dataGetterMap(m) {}
+		const DataGetter& find(const std::string& field_name) const {
+			if (auto it = dataGetterMap.find(field_name); it != end(dataGetterMap)) {
+				return it->second;
+			}
+			static DataGetter nullGetter{[](const Cdr&) { return GenericDatum{}; }};
+			return nullGetter;
+		}
+		
+		private:
+		DataGetterMap dataGetterMap;
+	};
+}
 
 #endif //_CDR_DICTIONARY_H_
