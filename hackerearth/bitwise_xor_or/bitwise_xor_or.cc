914e0bd1e14a32681eb49c42c7bb8b5f9ee2b9e9
Ankur Satle <sankurm@users.noreply.github.com>
2019-12-10 10:47:10 +0530

    Comment added

--- hackerearth/bitwise_xor_or/bitwise_xor_or.cc
+++ hackerearth/bitwise_xor_or/bitwise_xor_or.cc
@@ -13,6 +13,7 @@ int main() {
 	int x_or = nos.back();
 	int all_or = 0;
 	
+	//xor from rbegin gives xor for each subrange i.e. [i]th xor = xor of all nos[i] to nos[n-1]
 	std::for_each(nos.rbegin() + 1, nos.rend(), [&x_or, &all_or](const int n){
 	    x_or ^= n;
 	    all_or |= x_or;
