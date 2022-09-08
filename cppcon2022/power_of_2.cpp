1047d7fd77cb5a7ba3a0aa57aa31aa8e2523c190
Ankur Satle <sankurm@users.noreply.github.com>
2022-09-09 00:38:01 +0530

    constexpr, static_assert, main added

--- cppcon2022/power_of_2.cpp
+++ cppcon2022/power_of_2.cpp
@@ -1,34 +1,54 @@
 #include <bitset>
 #include <bit>
+#include <concepts>
 
-bool is_pow2_bit_counting_loop(unsigned n) {
+constexpr bool is_pow2_bit_counting_loop(std::unsigned_integral auto n) {
     int no_bits = 0;
-    for (auto i = 0u; i < sizeof(unsigned); ++i) {
+    for (auto i = 0u; i < sizeof(decltype(n)); ++i) {
         if ((n & (1u << i)) == n) ++no_bits;
     }
     return no_bits == 1;
 }
 
-bool is_pow2_match_with_powers(unsigned n) {
-    for (auto i = 0u; i < sizeof(unsigned); ++i) {
+constexpr bool is_pow2_match_with_powers(std::unsigned_integral auto n) {
+    for (auto i = 0u; i < sizeof(decltype(n)); ++i) {
         if (n == (1u << i)) return true;
     }
     return false;
 }
 
-bool is_pow2_knr(unsigned n) {
+constexpr bool is_pow2_knr(std::unsigned_integral auto n) {
   return (n & (n - 1)) == 0;
 }
 
-bool is_pow2_bitset(unsigned n) {
-    std::bitset<sizeof(unsigned)> bs(n);
+constexpr bool is_pow2_bitset(std::unsigned_integral auto n) {
+    std::bitset<sizeof(decltype(n))> bs(n);
     return bs.count() == 1;
 }
 
-bool is_pow2_popcount(unsigned n) {
+constexpr bool is_pow2_popcount(std::unsigned_integral auto n) {
     return std::popcount(n) == 1;
 }
 
-bool is_pow2_has_single_bit(unsigned n) {
+constexpr bool is_pow2_has_single_bit(std::unsigned_integral auto n) {
     return std::has_single_bit(n);
+}
+
+static_assert(is_pow2_bit_counting_loop(2u));
+static_assert(is_pow2_match_with_powers(4u));
+static_assert(is_pow2_knr(8u));
+//static_assert(is_pow2_bitset(16u));
+static_assert(is_pow2_popcount(32u));
+static_assert(is_pow2_has_single_bit(64u));
+    
+
+int main(int argc, char** argv) {
+    int ret = 0;
+    if (is_pow2_bit_counting_loop(2u)) ++ret;
+    if (is_pow2_match_with_powers(2u)) ++ret;
+    if (is_pow2_knr(2u)) ++ret;
+    if (is_pow2_bitset(2u)) ++ret;
+    if (is_pow2_popcount(2u)) ++ret;
+    if (is_pow2_has_single_bit(2u)) ++ret;
+    return ret;
 }
